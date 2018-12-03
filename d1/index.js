43ecd04641b0d4468cb34017844143a314075f4d
sevenindirecto <zabkar@gmail.com>
2018-12-03 23:11:13 +0100

    Day / Task 2

--- d1/index.js
+++ d1/index.js
@@ -1,18 +1,44 @@
 var fs = require('fs');
-// var lineReader = require('../shared/aoc-readline').lineReader;
 
 const input = 'input.txt';
-var contents = fs.readFileSync(input, 'utf8');
+const contents = fs.readFileSync(input, 'utf8');
+const commands = contents.split('\n').filter(v => v !== '');
 
-const commands = contents.split('\n');
-const sum = commands.reduce((acc, freq) => {
-  let op = freq.slice(0, 1);
-  let num = parseInt(freq.slice(1));
-  if(isNaN(num)) {
-    return acc;
+const addDelta = (base, delta) => {
+  let op = delta.slice(0, 1);
+  delta = parseInt(delta.slice(1));
+  
+  if (isNaN(delta)) {
+    return base;
   }
-  console.log(num);
-  return op === '-' ? acc - num : acc + num;
-}, 0);
+  
+  return op === '-' ? base - delta : base + delta;
+}
 
+let freq = 0;
+let duplicateFound = false;
+let i = 0;
+let commandLength = commands.length;
+let map = new Map();
+
+while (!duplicateFound) {
+  freq = addDelta(freq, commands[i]);
+  
+  if (map.has(freq)) {
+    duplicateFound = true;	  
+    break;
+  }
+  
+  map.set(freq, true);
+  i = i >= commandLength - 1 ? 0 : i + 1;
+}
+
+if (duplicateFound) {
+  console.log('Duplicate found at: ', freq);
+}
+else {
+  console.log('Duplicate NOT found');
+}
+
+const sum = commands.reduce((acc, freq) => addDelta(acc, freq), 0);
 console.log('Sum: ', sum);  
