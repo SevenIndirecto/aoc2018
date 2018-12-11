3e7207bec64a4817321f30a77db8b7e095f177d7
sevenindirecto <zabkar@gmail.com>
2018-12-11 01:22:39 +0100

    Day 5 / Star 2

--- d5/index.js
+++ d5/index.js
@@ -5,10 +5,10 @@ const polymer = fs.readFileSync(input, 'utf8').trim();
 
 // const polymer = 'dabAcCaCBAcCcaDA';
 
-const reactPolymer = polymer = {
+const reactPolymer = polymer => {
 	let newPoly = '';
 	for (let unit of polymer.slice(0)) {
-		if (newPfoly !== ''
+		if (newPoly !== ''
 			&& unit.toLowerCase() === newPoly.slice(-1).toLowerCase() 
 			&& unit !== newPoly.slice(-1) ) 
 		{
@@ -23,9 +23,23 @@ const reactPolymer = polymer = {
 console.log('In: ', polymer.length);
 console.log('Out:', reactPolymer(polymer).length);
 
-const getUnitTypes = polymer = {
-	new Set(polymer.toLowerCase().split(''));
+const getUnitTypes = polymer => {
+    return new Set(polymer.toLowerCase().split(''));
 }
 
 const unitTypes = getUnitTypes(polymer);
-console.log(unitTypes);
+
+let test = "abababaBBAABABAaba";
+const removeUnitType = (polymer, unitToRemove) => {
+    return polymer.split('').filter(unit => unit.toLowerCase() !== unitToRemove).join('');
+}
+
+let removedMap = [];
+
+for (unit of unitTypes) {
+    let removed = removeUnitType(polymer, unit);
+    removedMap.push(reactPolymer(removed).length);    
+}
+
+let minLength = removedMap.sort((a,b) => a - b)[0];
+console.log('Min length:', minLength);
