6fe4dee632eb0a0983829796247c78e016a9abcd
sevenindirecto <zabkar@gmail.com>
2018-12-13 23:37:38 +0100

    Day 7 / Star 2

--- d7/index.js
+++ d7/index.js
@@ -22,7 +22,8 @@ for (cmd of instructions) {
     }
 };
 
-let sortedMap = new Map([...steps.entries()].sort((a,b) => a[0] > b[0] ? 1 : -1));
+const initStepMap = () => new Map([...steps.entries()].sort((a,b) => a[0] > b[0] ? 1 : -1));
+let sortedMap = initStepMap();
 
 console.log(sortedMap);
 
@@ -52,4 +53,71 @@ while (executionOrder.length < sortedMap.size) {
     executeStep(step);
 }
 
-console.log(executionOrder);
+console.log('STAR 1: ', executionOrder);
+
+console.log('------------------ START STAR 2 **');
+sortedMap = initStepMap();
+
+const NUM_WORKERS = 5;
+const BASE_TASK_DURATION = 60;
+const BASE_CHAR_OFFSET = 'A'.charCodeAt(0) - 1;
+
+let workers = new Array(NUM_WORKERS).fill(0).map(() => ({startTime: 0, taskID: null}));
+executionOrder = '';
+
+const isTaskCompleted = (startTime, currentTime, taskID) => {
+    const taskLength = BASE_TASK_DURATION + taskID.charCodeAt(0) - BASE_CHAR_OFFSET;
+    return currentTime - startTime >= taskLength;
+}
+
+const processOngoing = (currentTime) => {
+    for (worker of workers) {
+        if (!worker.taskID) {
+            continue;
+        }
+
+        if (isTaskCompleted(worker.startTime, currentTime, worker.taskID)) {
+            executeStep(worker.taskID);
+            worker.taskID = null;
+        }
+    }
+}
+
+const getExecutableStepParallel = steps => {
+    let tasksInProgress = workers.reduce((acc, { taskID }) => taskID ? [...acc, taskID] : acc, []);
+    
+    for ([taskID, dependencies] of steps.entries()) {
+        if (dependencies.length < 1
+            && executionOrder.indexOf(taskID) === -1
+            && !tasksInProgress.includes(taskID))
+        {
+            return taskID;
+        }
+    }
+    return false;
+}
+
+let time = -1;
+while (executionOrder.length < sortedMap.size) {
+    time++; 
+    processOngoing(time);
+   
+    for (let i = 0; i < workers.length; i++) {
+        if (workers[i].taskID !== null) {
+            continue;
+        }
+
+        let task = getExecutableStepParallel(sortedMap);
+
+        if (task !== false) {
+            workers[i].startTime = time;
+            workers[i].taskID = task;
+        } else {
+            break;
+        }
+    }
+
+}
+
+console.log("Star 2:", executionOrder, 'Time:', time);
+
