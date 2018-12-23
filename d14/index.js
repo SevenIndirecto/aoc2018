c3a66cc3beed3db94ff85e3ccc74106b100a2797
sevenindirecto <zabkar@gmail.com>
2018-12-23 12:46:33 +0100

    Day 14 / Star 2 - Clusterfuck
    So this was a bit of a clusterfuck, spread over a couple of days.
    Initial Star 2 code was fine... apart from incorrectly checking
    if our string is contained, due to an 'optimization' for only checking
    the end... And so we miss the first occurence of our input 360781 due
    to the string actually ending with 3670810.
    
    ... So this set of a spiral of javascript memory / heap limit
    exploration etc. Bumped into an issue of maximum array size of about 50m
    numbers so implemented a layered array, which solved that issue.
    
    Found the result much of course and got the wrong result. Figured out
    the issue by running my input against another's internet solution to check for
    validity of input + expected output by puzzle.
    
    So in the end:
    * There was no need to worry about hitting array size limits
    * The data structure should have likely been a list
    * Elves should have just been 1 and 2, not an array but that was just
    speculation when doing Part 1

--- d14/index.js
+++ d14/index.js
@@ -10,120 +10,85 @@ class Elf {
     }
 }
 
-let elves = [new Elf(0), new Elf(1)];
-// let scoreboard = [3, 7];
+let elves = [new Elf({i1: 0, i2: 0}), new Elf({i1: 0, i2: 1})];
+// Splitting into multiple arrays due to v8 limits.
+// This was... due to a wrong implementation of 'ends with string checking' instead of contains.
+let scoreboards = [[3,7]];
 
-let scoreboards = [];
-scoreboards.push([3,7]);
-
-
+const scoreboardLength = () => {
+    return (scoreboards.length - 1) * ARRAY_SIZE_LIMIT + scoreboards[scoreboards.length - 1].length;
+}
 
 const createNewRecipes = (currentRecipes) => {
-//     log.start();
     let sum = currentRecipes.reduce((sum, recipe) => sum + recipe, 0);
 
     return `${sum}`.split('').map(digit => parseInt(digit));
 }
 
 const pickNewRecipe = (elf) => {
-    
+    let index = elf.currentRecipe.i1 * ARRAY_SIZE_LIMIT + elf.currentRecipe.i2;
+    index += 1;
+    index = (index + scoreboards[elf.currentRecipe.i1][elf.currentRecipe.i2]) % scoreboardLength();
 
-
-    let index = (elf.currentRecipe + 1 + scoreboard[elf.currentRecipe]) % scoreboard.length;
-    return index;
-}
-
-const drawState = () => {
-    let recipeMap = {};
-    for (let i = 0; i < elves.length; i++) {
-        if (recipeMap[elves[i].currentRecipe]) {
-            recipeMap[elves[i].currentRecipe].push(i);
-        } else {
-            recipeMap[elves[i].currentRecipe] = [i];
-        }
+    // Convert to split array
+    return {
+        i1: Math.floor(index / ARRAY_SIZE_LIMIT),
+        i2: index % ARRAY_SIZE_LIMIT,
     }
-    let out = '';
-    for (let i = 0; i < scoreboard.length; i++) {
-        let delims;
-        if (recipeMap[i]) {
-            delims = (recipeMap[i] % 2) === 1 ? ['[',']'] : ['(',')'];
-        }  else {
-            delims = [' ', ' '];
-        }
-        out += `${delims[0]}${scoreboard[i]}${delims[1]}`;
-    }
-    console.log(out);
 }
 
 const getCurrentRecipes = () => {
     return elves.map(elf => {
-       return scoreboard[elf.currentRecipe] 
+       return scoreboards[elf.currentRecipe.i1][elf.currentRecipe.i2]; 
     });
 }
 
-drawState();
 const processTick = () => {
-   // log.start();
     let newRecipes = createNewRecipes(getCurrentRecipes());
-    //log.stop('New recipes');
-    //log.start();
+    
     for (recipe of newRecipes) {
-        scoreboard.push(recipe);
+        if (scoreboards[scoreboards.length - 1].length === ARRAY_SIZE_LIMIT) {
+            scoreboards.push([recipe]);
+        }
+        else {
+            scoreboards[scoreboards.length - 1].push(recipe);
+        }
     }
-    // scoreboard = [...scoreboard, ...newRecipes];
-   // log.stop('Merge scoreboards');
-    //log.start();
+    
     for (let i = 0; i < elves.length; i++) {
         elves[i].currentRecipe = pickNewRecipe(elves[i]);
     }
-    if (elves[0].currentRecipe < 100 || elves[0].currentRecipe < 100) {
-        console.log(elves, scoreboard.length, 
-            require('v8').getHeapStatistics().total_available_size / 1024 / 1024);
-    }
-    //log.stop('Pick new recipe');
-//    drawState();
 }
 
-const scoreboardEndsWithNeedle = () => {
+const scoreboardContainsNeedle = () => {
     let num = '';
-    if (scoreboard.length < NUM_TO_APPEAR_LENGTH) {
+    if (scoreboardLength() < NUM_TO_APPEAR_LENGTH + 1) {
         return false;
     }
 
-    for (let i = NUM_TO_APPEAR_LENGTH; i > 0; i--) {
-        num += scoreboard[scoreboard.length - i];
+    // Let's assume the digits we're looking for can only be present
+    // in the last 2 scoreboards
+    for (let i = NUM_TO_APPEAR_LENGTH + 1; i > 0; i--) {
+        if (i > scoreboards[scoreboards.length - 1].length) {
+            // Look into previous scoreboard
+            num += 
+                scoreboards[scoreboards.length - 2][scoreboards[scoreboards.length - 2].length 
+                    - (i - scoreboards[scoreboards.length - 1].length)];
+        }
+        else {
+            num += 
+                scoreboards[scoreboards.length - 1][scoreboards[
+                    scoreboards.length - 1].length - i];
+        }
     }
 
-    return NUM_TO_APPEAR === num;
+    return num.includes(NUM_TO_APPEAR);
 }
 
-class Logger {
-    constructor() {
-        this.startTime = 0;
-    }
-    
-    start() {
-        this.startTime = Date.now();
-    }
-
-    getTimeDiff() {
-        return Date.now() - this.startTime;
-    }
-
-    stop(tag = '') {
-        console.log(`${tag} [${this.getTimeDiff()} ms]`);
-    }
+while (!scoreboardContainsNeedle()) {
+    processTick();
 }
 
-const log = new Logger();
-const logInner = new Logger();
-
-while (!scoreboardEndsWithNeedle()) {
-    processTick()
-    //logInner.stop('Inner log');
-    //logInner.start();
-    //console.log(scoreboard.length);
-}
-
-console.log('Appears after:', scoreboard.length - NUM_TO_APPEAR_LENGTH);
+console.log('Appears after:', scoreboardLength() - NUM_TO_APPEAR_LENGTH - 1, 
+    scoreboardLength() - NUM_TO_APPEAR_LENGTH);
 // console.log(scoreboard.splice(RECIPE_BEFORE, 10).join(''));
