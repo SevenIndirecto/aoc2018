4427507a231802b8f6b2f45a43c91e775781436f
sevenindirecto <zabkar@gmail.com>
2018-12-25 22:33:56 +0100

    Day 15 / Star 1

--- d15/index.js
+++ d15/index.js
@@ -1,34 +1,50 @@
 const fs = require('fs');
 
-const inputFile = 'input-test2.txt';
+const inputFile = process.argv[2] || 'input.txt';
 let input = fs.readFileSync(inputFile, 'utf8').trim().split('\n');
 
 class Unit {
     constructor(x, y) {
-        this.x = x;
         this.y = y;
+        this.x = x;
+        this.hp = 200;
+        this.attackPower = 3;
     }
 
-    getTargetInRange() {
+    getTargetsInRange() {
         let targetSquares = [
             [this.y - 1, this.x], [this.y, this.x - 1], 
             [this.y, this.x + 1], [this.y + 1, this.x]
         ];
         
+        let targets = [];        
+
         for (let [y,x] of targetSquares) {
-            console.log('blah', y, x, this.enemy);
-            if (squareContainsAUnit(x, y, this.enemy)) {
-                return [y,x];
+            if (squareContainsAUnit([y, x], this.enemy)) {
+                targets = [...targets, ...units.filter(u => u.y === y && u.x === x && u.isAlive())];
             }
         }
 
-        return false;
+        return targets;
+    }
+
+    getSquare() {
+        return [this.y, this.x];
+    }
+
+    setSquare([y, x]) {
+        this.x = x;
+        this.y = y;
+    }
+
+    isAlive() {
+        return this.hp > 0;
     }
 }
 class Elf extends Unit {
     constructor(x,y) {
         super(x,y);
-        this.enemy = 'Goblin';
+        this.enemy = 'Goblin';       
     }
 }
 class Goblin extends Unit {
@@ -59,9 +75,9 @@ for (let y = 0; y < input.length; y++) {
     }
 }
 
-const squareContainsAUnit = (x, y, unitType = false) => {
+const squareContainsAUnit = ([y, x], unitType = false) => {
     for (unit of units) {
-        if (unit.x === x && unit.y === y) {
+        if (unit.x === x && unit.y === y && unit.isAlive()) {
             if (unitType !== false ) {
                 return unit.constructor.name === unitType;
             }
@@ -71,65 +87,53 @@ const squareContainsAUnit = (x, y, unitType = false) => {
     return false;
 }
 
-let checkedSquares = [];
+// Reset on each target square iteration when processing in 'processTurn()'
+const checked = new Map();
 
-const getPathLengthToSquare = ([sy, sx], [ty, tx], depth, shortestPath = false) => {
-    if (shortestPath !== false && depth >= shortestPath) {
-        console.log('Depth charge triggered');
-        return false;
-    }
-    console.log(depth, 'Check path from ', sy, sx, 'to', ty, tx);
-    if (checkedSquares.filter(([cy, cx]) => cy === sy && cx === sx).length > 0) {
-        console.log(depth, 'Checked square');
-        return false;
+const findShortestPathFromTo = ([ay, ax], [by, bx], depth) => {
+    if (ay === by && ax === bx) {
+        return [[ay,ax]];
     }
 
-    checkedSquares.push([sy, sx]);
-
-    // Hit
-    if (sy === ty && sx === tx) {
-        console.log(depth, 'HIT');
-        return 0;
+    if (ay < 0 || ax < 0) {         
+        return false;
     }
 
-    // Is illegal move?
-    if (squareContainsAUnit(sx, sy) || map[sy][sx] === '#') {
-        console.log(depth, 'Illegal move');
+    if (depth > 0 && (map[ay][ax] === '#' || squareContainsAUnit([ay, ax]))) {       
         return false;
+    }
+
+    let key = `${ay}-${ax}`;
+    if (checked.has(key) && checked.get(key) <= depth) {
+        return false;
+    }
+    checked.set(key, depth);
+
+    let scanOrder = [[-1, 0], [0, -1], [0, 1], [1,0]];
+    let shortestPath = false;
+
+    for (delta of scanOrder) {
+        let path = findShortestPathFromTo([ay + delta[0], ax + delta[1]], [by, bx], depth + 1);
+
+        if (path !== false && (shortestPath === false || shortestPath.length > path.length)) {
+            shortestPath = path;
+        }
+    }
+
+    if (shortestPath !== false) {
+        shortestPath.push([ay, ax]);
     }
     
-    // Get possible moves
-    let moves = [[sy - 1, sx], [sy, sx - 1], [sy, sx + 1], [sy + 1, sx]];
-    
-    let pathLength = false;
-    for ([my, mx] of moves) {
-        let movePathLength = getPathLengthToSquare([my, mx], [ty, tx], depth + 1, pathLength);
-        if (movePathLength === 0) {
-            console.log(depth, 'Path Return 1');
-            return 1;
-        }
-
-        if (movePathLength === false) {
-            continue;
-        }
-        
-        if (pathLength === false || movePathLength < pathLength) {
-            pathLength = movePathLength;
-        }
-    }
-    // console.log('Checked squares', checkedSquares);
-    console.log(depth, 'Return path', pathLength);
-
-    return pathLength === false ? false : pathLength + 1;
+    return shortestPath;
 }
 
 const drawMap = () => {
     for (let y = 0; y < map.length; y++) {
         let line = '';
         for (let x = 0; x < map[y].length; x++) {
-            if (squareContainsAUnit(x, y, 'Elf')) {
+            if (squareContainsAUnit([y, x], 'Elf')) {
                 line += 'E';
-            } else if (squareContainsAUnit(x, y, 'Goblin')) {
+            } else if (squareContainsAUnit([y, x], 'Goblin')) {
                 line += 'G';
             } else {
                 line += map[y][x];
@@ -139,29 +143,33 @@ const drawMap = () => {
     }
 }
 
+let turn = 0;
 const processTurn = () => {
+    turn++;
+    console.log('START Processing turn', turn);
 
-    for (let unitIndex = 0; unitIndex < units.length; unitIndex++) {
+    for (let unitIndex = 0; unitIndex < units.length; unitIndex++) {        
         let unit = units[unitIndex];
-        drawMap();
-        console.log('Processing unit: ', unit);
+
+        if (!unit.isAlive()) {
+            continue;
+        }
         
         // Identify all possible targets
         let squaresInRange = [];
         let enemy = unit.enemy;
-        enemies = units.filter(target => target.constructor.name === enemy);
+        enemies = units.filter(target => target.constructor.name === enemy && target.isAlive());
 
         // Identify open squares in range of target, 1 square P/DOWN/LEFT/RIGHT of target
         let openSquares = [];
         enemies.forEach(enemy => {
             let x = enemy.x;
             let y = enemy.y;
-            console.log(enemy); 
             let squares = [[y-1,x], [y,x-1], [y,x+1], [y+1,x]];
             squares.forEach(([y,x]) => {
                 if (y < 0 || x < 0) return false;
 
-                if (!squareContainsAUnit(x, y) 
+                if (!squareContainsAUnit([y, x]) 
                     && map[y][x] !== '#' 
                     && openSquares.filter(([sy,sx]) => sx === x && sy === y).length < 1) {
                     openSquares.push([y,x]);
@@ -169,84 +177,68 @@ const processTurn = () => {
             });
         });
 
-        // Is in range of a target?
-        let target = unit.getTargetInRange();
-        if (target) {
-           console.log('TARGET IN RANGE');
-           continue;
-           // TODO: Attack target
-           // console.log('Target in range', target);          
-        }
+        // Move if no target in range
+        if (unit.getTargetsInRange().length < 1) {
+            let shortestPath = false;
 
-        let moves = [
-            [unit.y - 1, unit.x], [unit.y, unit.x - 1], 
-            [unit.y, unit.x + 1], [unit.y + 1, unit.x]
-        ];
-
-        
-        let totalShortestMove = false;
-        let totalShortestMoveIndex = -1;
-
-        for (targetSquare of openSquares) {
-            let shortestMove = false;            
-            let shortestMoveIndex = -1;
-
-console.log('Target square', targetSquare);
-
-            let move = getPathLengthToSquare([unit.y, unit.x], targetSquare, 0, totalShortestMove);
-            
-            if (move !== false && (totalShortestMove === false || move < totalShortestMove) {
-                totalShortestMove = move;
+            for (let target of openSquares) {
+                checked.clear();
+                let path = findShortestPathFromTo(unit.getSquare(), target, 0);
+                if (path !== false && (shortestPath === false || path.length < shortestPath.length)) {
+                    shortestPath = path;
+                }
             }
             
-            continue;
-            // Move to target
-            let i = 0;
-            for ([my, mx] of moves) {
-                checkedSquares = [];
-                if (my < 0 || mx < 0) {
-                    continue;
-                }
-
-                let move = getPathLengthToSquare([my, mx], targetSquare, 0);
-                console.log('Move', move);
-                if (move !== false) {
-                    move++;
-                }
-                console.log('Actual move', move);
-                if ((shortestMove === false && move !== false) 
-                    || (move !== false && move < shortestMove)) {
-                    shortestMove = move;
-                    shortestMoveIndex = i;
-                }
-    
-                i++;
-            }
-            
-            if (shortestMove !== false) {
-                shortestMove +=1;
-            }
-
-
-            if (shortestMove !== false 
-                && (totalShortestMove === false || shortestMove < totalShortestMove)) {
-
-                totalShortestMove = shortestMove;
-                totalShortestMoveIndex = shortestMoveIndex;
+            if (shortestPath !== false) {
+                // Pop twice, since last element in path is itself;
+                shortestPath.pop();            
+                unit.setSquare(shortestPath.pop());
             }
         }
 
-        console.log('Unit pre move:', unit, totalShortestMove);
-        if (totalShortestMove) { 
-                unit.y = moves[totalShortestMoveIndex][0];            
-                unit.x = moves[totalShortestMoveIndex][1];  
+        let targets = unit.getTargetsInRange();
+        if (targets.length > 0) {            
+            let lowestHP = targets.reduce(
+                (lowest, enemy) => {
+                    if (lowest.hp < enemy.hp) {
+                        return lowest;
+                    } else if (lowest.hp === enemy.hp) {
+                        if (lowest.y < enemy.y) {
+                            return lowest;
+                        } 
+                        if (lowest.y === enemy.y && lowest.x < enemy.x) {
+                            return lowest;
+                        }
+                        return enemy;
+                    }
+                    return enemy;                    
+                },
+                { hp: 201, x: 20000, y: 20000 }
+            );
+
+            lowestHP.hp -= unit.attackPower;
         }
-        console.log('Unit post move: ', unit, totalShortestMove);
-    };
+    }
+
+    units.sort((u1, u2) => {
+        if (u1.y < u2.y || (u1.y === u2.y && u1.x < u2.x)) {
+            return -1;
+        }
+        return 1;
+    });
+    console.log('END Processing turn', turn);
 }
 
+const groupWins = (group) => units.filter(u => u.enemy === group && u.isAlive()).length < 1;
+
 drawMap();
-processTurn();
-drawMap();
-// processTurn();
-// drawMap();
+do {    
+    processTurn();
+    drawMap();
+    console.log(units);
+} while (!groupWins('Elf') && !groupWins('Goblin'))
+
+let totalHP = units.filter(u => u.isAlive()).reduce((sum, u) => u.hp + sum, 0);
+console.log('Outcome: ', (turn - 1) * totalHP, 'Total HP', totalHP);
+console.log('Winners: ', groupWins('Elf') ? 'Elves' : 'Goblins');
+
